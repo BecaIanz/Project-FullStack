729c6251d319d1a977655e661a6be0c0a8a5a79d
BecaIanz <silveiraianzr@gmail.com>
2026-06-03 16:57:42 -0300

    feat: add image in card component #5

--- Patinhas/Patinhas.Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
+++ Patinhas/Patinhas.Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbe027f265b7ba87b4e054138aa8411674bb2aa8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+755731f4d66d8572a14d3ca65b26ab12c6f574be")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
