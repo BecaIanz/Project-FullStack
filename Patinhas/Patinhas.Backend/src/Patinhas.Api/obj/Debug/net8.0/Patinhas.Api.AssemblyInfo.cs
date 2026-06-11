d5fb22b60ad490b50dc48fd5ce02aa58f31805a4
FehFialho <fernandaklechowicz@gmail.com>
2026-06-11 13:41:12 -0300

    fix: corrected the sln version (slnx to sln) #12

--- Patinhas/Patinhas.Backend/src/Patinhas.Api/obj/Debug/net8.0/Patinhas.Api.AssemblyInfo.cs
+++ Patinhas/Patinhas.Backend/src/Patinhas.Api/obj/Debug/net8.0/Patinhas.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Patinhas.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3dfb46a00f15e6dd629fc79a12903678f28fe7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba4494b09bbd11c4ac35bb613f4710f9988169a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Patinhas.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Patinhas.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
