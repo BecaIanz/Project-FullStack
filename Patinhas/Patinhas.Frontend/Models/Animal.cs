cced74aacd5dca70abbecc1e4ba9888e538f39f0
BecaIanz <silveiraianzr@gmail.com>
2026-06-18 16:56:59 -0300

    feat: add Animal model

---
+++ Patinhas/Patinhas.Frontend/Models/Animal.cs
@@ -0,0 +1,10 @@
+namespace Frontend.Models;
+
+public record Animal(
+    string Name,
+    List<string> Images,
+    string Description,
+    string Breed,
+    string Age,
+    string Sex
+);
--- Patinhas/Patinhas.Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
+++ Patinhas/Patinhas.Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df1811ebb4a933d2c21789a3d6877499d0d7f4e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6904b154b7358a27e58412d50246f00de8cfd54f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
