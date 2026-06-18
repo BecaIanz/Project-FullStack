6b67377cc19976dacee6ef801eb779e475114056
FehFialho <fernandaklechowicz@gmail.com>
2026-06-18 16:56:57 -0300

    refact: update animal features

--- Patinhas/Patinhas.Backend/src/Patinhas.Application/IServices/IAnimalService.cs
+++ Patinhas/Patinhas.Backend/src/Patinhas.Application/IServices/IAnimalService.cs
@@ -4,8 +4,11 @@ using Patinhas.Backend.Domain.Entities;
 public interface IAnimalService
 {
     Task<Animal> FindById(int id);
-    Task<int> CreateAnimal(Animal animal);
+    Task<List<Animal>> GetAll();
+    Task<List<AnimalFoto>> GetPhotos(int animalId);
 
+    // CRUD
+    Task<int> CreateAnimal(Animal animal);
     Task<bool> DeleteAnimal(Animal animal);
 
     Task<bool> UpdateAnimal(int id, Animal animal);
