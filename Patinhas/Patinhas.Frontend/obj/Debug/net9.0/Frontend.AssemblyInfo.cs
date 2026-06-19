863ded1be5abe257d93487e2d417f1ea4c744603
BecaIanz <silveiraianzr@gmail.com>
2026-06-19 11:18:09 -0300

    feat: create and completed LoginPage and completed HomePage

--- Patinhas/Patinhas.Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
+++ Patinhas/Patinhas.Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6904b154b7358a27e58412d50246f00de8cfd54f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cced74aacd5dca70abbecc1e4ba9888e538f39f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
