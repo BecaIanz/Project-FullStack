1782f15330eb5698cfa04cb3f77d1012046bac8c
BecaIanz <silveiraianzr@gmail.com>
2026-06-19 16:34:12 -0300

    feat: complete petsCurtidos Page #9 #11

--- Patinhas/Patinhas.Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
+++ Patinhas/Patinhas.Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a0ceae28cf96d91c219ab66a6242f6871da1ccb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+072a4a54746f2f031671b577f80bf5e3f0e3d821")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
