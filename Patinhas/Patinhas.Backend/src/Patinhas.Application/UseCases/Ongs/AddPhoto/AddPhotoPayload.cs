84ea8e75937e3c674c9448d59b2a44683caf0403
LaysArceles <LaysArceles12@gmail.com>
2026-06-19 08:50:57 -0300

    feat: add Use Cases photo #2

--- Patinhas/Patinhas.Backend/src/Patinhas.Application/UseCases/Ongs/AddPhoto/AddPhotoPayload.cs
+++ Patinhas/Patinhas.Backend/src/Patinhas.Application/UseCases/Ongs/AddPhoto/AddPhotoPayload.cs
@@ -1,6 +1,6 @@
 namespace Patinhas.Backend.Application.UseCases.Ongs.AddPhoto;
 public record AddPhotoPayload
 {
-    public int IdAnimal {get;set;}
+    public int IdOng {get;set;}
     public string Url {get;set;}
 }
